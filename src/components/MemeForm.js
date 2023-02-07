a34067ce115121facaec672de3f1aacfcedb29d7
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-07 23:50:57 +0100

    polish styling

--- src/components/MemeForm.js
+++ src/components/MemeForm.js
@@ -4,8 +4,16 @@ const MemeForm = () => {
   return (
     <main>
       <form className={styles.form}>
-        <input className={styles.form__input} type="text" />
-        <input className={styles.form__input} type="text" />
+        <input
+          className={styles.form__input}
+          type="text"
+          placeholder="First text"
+        />
+        <input
+          className={styles.form__input}
+          type="text"
+          placeholder="Second text"
+        />
         <button className={styles.form__button}> Get a new meme image</button>
       </form>
     </main>
