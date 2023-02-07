3a670a7c46cf9e4bdb2babe61262a8860facfd13
JerryWski <jarek_wasilewski@yahoo.com>
2023-02-07 16:44:41 +0100

    cleaning stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import Card from "./components/Card";
-import Header from "./components/Header";
-import offerData from "./data"
+
 
 const App = () => {
-  const dataElements = offerData.map((data)=> {
-    return <Card
-      key={data.id} {...data}
-    />
-   
-  })
-  return (
-    <div>
-      <Header />
-      {dataElements}
-    </div>
-  );
+
 };
 
 export default App;
